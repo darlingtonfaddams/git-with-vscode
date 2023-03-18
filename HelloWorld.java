32c79b7cfd57af7706ba99db223199410309e7de
Darlington Faddams <darlingtonfaddams@gmail.com>
2023-03-18 05:53:45 +0000

    made changes to hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         String hello = "Hello, world!";
+        int age = 22;
 
         System.out.println(hello);
+        System.out.println("The man is " + age + " years old.");
 
     }
 }
